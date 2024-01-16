5b091411837a72fe2f67d28558df81222307b7ad
Ajibade Gafar Opeyemi <eco.ajibade@gmail.om>
2024-01-15 20:02:23 -0800

    0x00. Python - Hello, World

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print("programning is like building a multilingual puzzle")
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+are2 = "School"
+str1 +="" + str2
+print("Welcome to {}!".format_str1))
