85c0ca58e8f488b4c13d1762cec6fe3c5a3ab55c
oceanflow1990 <eco.ajibade@gmail.com>
2024-01-22 14:44:31 -0800

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/pyhton3
-def max_integer(my_list=[]):
-    new_list = []
-    if my_list:
-        my_list.sort(reverse=True)
-        return (my_list[0])
-    return (None)
+def max_integer(my_list=):
+     if len(my_list) == 0:
+          return ("None")
+      x = my_list[0]
+       for i in my_list:
+            if i > x:
+                 x = i
+                  return (x)
